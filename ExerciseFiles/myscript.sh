507ce7ec9aa3d2a3037dd23118b2b3f28c1a34f6
jvliu521 <148154872+jvliu521@users.noreply.github.com>
2026-02-25 23:56:49 +0000

    update on color

--- ExerciseFiles/myscript.sh
+++ ExerciseFiles/myscript.sh
@@ -1,1 +1,9 @@
-echo "Hello, world!"
+#!/usr/bin/env bash
+
+echo -e "\033[33;44mColor Text\033[0m"
+echo -e "\033[30;42mColor Text\033[0m"
+echo -e "\033[41;105mColor Text"
+echo "some text that shouldn't have styling"
+echo -e "\033[0m"
+echo "some text that shouldn't have styling"
+echo -e "\033[4;31;40mERROR:\033[0m\033[31;40m Something went wrong.\033[0m"
